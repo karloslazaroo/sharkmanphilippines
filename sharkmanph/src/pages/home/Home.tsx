425b9e7cdaf10d9c7b8079671a900fa24779576d
karloslazaroo <101194055+karloslazaroo@users.noreply.github.com>
2023-05-22 02:30:28 +0800

    push

---
+++ sharkmanph/src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
+
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+        Home
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
